e30332ea4e327deb27a5cb3a13467bda8130d921
Erin Hoops <eehoops@gmail.com>
2017-04-18 12:16:37 -0700

    adding bootstrap content for bundling

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const App = require('./App');
+import Bootstrap from '../bootstrap/css/bootstrap.min.css';
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,34 @@ module.exports = {
   entry: __dirname + '/app/index.js',
   module: {
     loaders: [
+
+      { test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader?limit=10000&mimetype=image/svg+xml'
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
